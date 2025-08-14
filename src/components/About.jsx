f14acfd82a41964ec2f1a8598c6f57ac2c865f8e
Vaibhav Deval <160069545+vaibhav-deval@users.noreply.github.com>
2025-08-14 23:03:20 +0530

    Update About.jsx
    about updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <center><p className="text-lg max-w-2xl text-center ">
           <ReactTyped
                     strings={[
-                      "Motivated and detail-oriented BCA student with a passion for frontend development and user-centered design. Eager to apply my skills in HTML, CSS, JavaScript, React, and modern frameworks like Tailwind CSS to real-world projects. Seeking a junior frontend developer role where I can contribute to innovative solutions and grow within a collaborative tech environment.",
+                      "A detail-oriented and passionate Frontend Developer with a BCA degree and hands-on experience in building clean, responsive, and interactive user interfaces. Proficient in React.js, JavaScript, and Tailwind CSS and enthusiastic about contributing to dynamic web development teams. Open to frontend or full-stack roles that foster learning and collaboration. ",
                     ]}
                     typeSpeed={20}
                     backSpeed={0}
